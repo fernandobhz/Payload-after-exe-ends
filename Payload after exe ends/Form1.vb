e8e1e9b05f4762873f84078c9e4b2e44553370f6
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:13:53 -0300

    old project migrated

---
+++ Payload after exe ends/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Function EXEPayload() As String
+        Dim Contents = My.Computer.FileSystem.ReadAllText(Application.ExecutablePath)
+
+        If Contents.EndsWith("}") Then
+            Dim Reverse As String = String.Join("", Contents.Reverse)
+            Dim Point = Reverse.IndexOf("{")
+
+            If Point < 0 Then
+                Return Nothing
+            Else
+                Point = Contents.Length - Point - 1
+
+                Dim Value = Contents.Substring(Point)
+                Return Value
+            End If
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox(EXEPayload)
+        Application.Exit()
+    End Sub
+End Class
